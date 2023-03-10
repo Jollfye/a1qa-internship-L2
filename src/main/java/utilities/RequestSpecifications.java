8fe03fb2a8dc52f55b10ff665cb84b85d251d8cc
a.koshelev <a.koshelev@qa-academy.by>
2023-03-10 22:29:05 +0600

    Add commonGiven

---
+++ src/main/java/utilities/RequestSpecifications.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.experimental.UtilityClass;
+import utilities.configuration.Configuration;
+
+import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.given;
+
+@UtilityClass
+public class RequestSpecifications {
+    public static RequestSpecification commonGiven() {
+        return given()
+                .baseUri(Configuration.getApiUrl())
+                .contentType(ContentType.JSON.withCharset(StandardCharsets.UTF_8));
+    }
+}
