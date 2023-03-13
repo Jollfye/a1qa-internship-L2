9682b1ad9950616358e4439e614cab331bf70816
a.koshelev <a.koshelev@qa-academy.by>
2023-03-13 23:35:37 +0600

    Update post model to use fluent accessors

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -2,12 +2,30 @@ package models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(fluent = true)
 public class Post {
-    private int userId;
-    private int id;
+    private Integer userId;
+    private Integer id;
     private String title;
     @JsonProperty("body")
     private String postBody;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPostBody() {
+        return postBody;
+    }
 }
