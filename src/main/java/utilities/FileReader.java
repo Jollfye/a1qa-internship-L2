90a9dbd29f96f1a7682e556c2426a9202b16b7f8
a.koshelev <a.koshelev@qa-academy.by>
2023-03-13 23:41:12 +0600

    Update FileReader to work with resources and read content

--- src/main/java/utilities/FileReader.java
+++ src/main/java/utilities/FileReader.java
@@ -1,12 +1,46 @@
 package utilities;
 
-import aquality.selenium.core.utilities.ISettingsFile;
-import aquality.selenium.core.utilities.JsonSettingsFile;
 import lombok.experimental.UtilityClass;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 @UtilityClass
 public class FileReader {
-    public static ISettingsFile getSettingsResourceFile(String resource) {
-        return new JsonSettingsFile(resource);
+    public static File getResourceFile(String resource) {
+        return new File(getResourcePath(resource));
+    }
+
+    public static String getResourcePath(final String resourceName) {
+        try {
+            URL resourceURL = FileReader.class.getClassLoader().getResource(resourceName);
+            return Objects.requireNonNull(resourceURL).getPath();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException(
+                    String.format("Resource file %1$s was not found or cannot be loaded", resourceName), e);
+        }
+    }
+
+    public static String getCanonicalPath(File file) {
+        try {
+            return file.getCanonicalPath();
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error getting canonical path for file: "
+                    + file.getAbsolutePath(), e);
+        }
+    }
+
+    public static String getFileContent(String filename) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filename)));
+        } catch (IOException e) {
+            throw new UncheckedIOException(
+                    String.format("Content of file %1$s can't be read as String", filename), e);
+        }
     }
 }
