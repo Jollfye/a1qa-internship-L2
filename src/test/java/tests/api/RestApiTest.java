cbd1ee3d4574838ce685514ec426b8be10d5d2d7
a.koshelev <a.koshelev@qa-academy.by>
2023-03-13 23:48:03 +0600

    Update rest api test to include all required steps

--- src/test/java/tests/api/RestApiTest.java
+++ src/test/java/tests/api/RestApiTest.java
@@ -2,6 +2,7 @@ package tests.api;
 
 import org.testng.annotations.Test;
 import steps.api.RestApiSteps;
+import utilities.configuration.TestDataProvider;
 
 public class RestApiTest {
     private final RestApiSteps restApiSteps = new RestApiSteps();
@@ -9,5 +10,10 @@ public class RestApiTest {
     @Test
     public void testRestApi() {
         restApiSteps.getPosts();
+        restApiSteps.getPostById(TestDataProvider.getPostId());
+        restApiSteps.getPostByIdWithResponseBodyEmpty(TestDataProvider.getPostIdWithResponseBodyEmpty());
+        restApiSteps.postWithUserId(TestDataProvider.getPostRequestUserId());
+        restApiSteps.getUsers();
+        restApiSteps.getUserById(TestDataProvider.getTestUserId());
     }
 }
