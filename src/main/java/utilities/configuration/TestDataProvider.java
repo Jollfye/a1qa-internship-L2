285392ab89c4344d6e7b1c732c1a61582afc4c84
a.koshelev <a.koshelev@qa-academy.by>
2023-03-15 01:18:56 +0600

    Separate rest api test data into different tests' data

--- src/main/java/utilities/configuration/TestDataProvider.java
+++ src/main/java/utilities/configuration/TestDataProvider.java
@@ -8,37 +8,41 @@ import utilities.JsonReader;
 
 @UtilityClass
 public class TestDataProvider {
-    private static final ISettingsFile dataSettingsResourceFile =
-            JsonReader.getSettingsResourceFile("RestApiTestData.json");
-    private static final String userResourceFileContent = FileReader.getFileContent(
+    private static final ISettingsFile getPostWithIdTestData =
+            JsonReader.getSettingsResourceFile("getPostWithIdTestData.json");
+    private static final ISettingsFile getPostWithResponseBodyEmptyTestData =
+            JsonReader.getSettingsResourceFile("getPostWithResponseBodyEmptyTestData.json");
+    private static final ISettingsFile createPostTestData =
+            JsonReader.getSettingsResourceFile("createPostTestData.json");
+    private static final String testUser = FileReader.getFileContent(
             FileReader.getCanonicalPath(
-                    FileReader.getResourceFile("RestApiTestUser.json")));
+                    FileReader.getResourceFile("testUser.json")));
 
     public static int getPostId() {
-        return (int) dataSettingsResourceFile.getValue("/postId");
+        return (int) getPostWithIdTestData.getValue("/postId");
     }
 
     public static int getPostUserId() {
-        return (int) dataSettingsResourceFile.getValue("/postUserId");
+        return (int) getPostWithIdTestData.getValue("/postUserId");
     }
 
     public static int getPostIdWithResponseBodyEmpty() {
-        return (int) dataSettingsResourceFile.getValue("/postIdWithResponseBodyEmpty");
+        return (int) getPostWithResponseBodyEmptyTestData.getValue("/postIdWithResponseBodyEmpty");
     }
 
     public static int getPostRequestUserId() {
-        return (int) dataSettingsResourceFile.getValue("/postRequestUserId");
+        return (int) createPostTestData.getValue("/postRequestUserId");
     }
 
     public static int getRandomAlphanumericLength() {
-        return (int) dataSettingsResourceFile.getValue("/randomAlphanumericLength");
-    }
-
-    public static User getTestUser() {
-        return JsonReader.getValueTypeFromJson(userResourceFileContent, User.class);
+        return (int) createPostTestData.getValue("/randomAlphanumericLength");
     }
 
     public static int getTestUserId() {
-        return (int) dataSettingsResourceFile.getValue("/testUserId");
+        return getTestUser().getId();
+    }
+
+    public static User getTestUser() {
+        return JsonReader.getValueTypeFromJson(testUser, User.class);
     }
 }
