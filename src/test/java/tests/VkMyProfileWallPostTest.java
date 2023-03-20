ec14d46424cc69e3fd4c4facdbe9cab65df779e5
a.koshelev <a.koshelev@qa-academy.by>
2023-03-21 05:47:36 +0600

    Update VkMyProfileWallPostTest to use VkApiUtils

--- src/test/java/tests/VkMyProfileWallPostTest.java
+++ src/test/java/tests/VkMyProfileWallPostTest.java
@@ -5,13 +5,13 @@ import models.Photo;
 import models.Post;
 import models.User;
 import org.testng.annotations.Test;
-import steps.api.VkApiSteps;
-import steps.ui.MyProfilePageSteps;
-import steps.ui.NewsPageSteps;
-import steps.ui.SignInVerificationPageSteps;
-import steps.ui.WelcomePageSteps;
+import steps.MyProfilePageSteps;
+import steps.NewsPageSteps;
+import steps.SignInVerificationPageSteps;
+import steps.WelcomePageSteps;
 import utilities.GetModelInstanceUtils;
 import utilities.RandomUtils;
+import utilities.api.VkApiUtils;
 import utilities.configuration.TestDataProvider;
 
 public class VkMyProfileWallPostTest extends BaseTest {
@@ -19,7 +19,6 @@ public class VkMyProfileWallPostTest extends BaseTest {
     private final SignInVerificationPageSteps signInVerificationPageSteps =
             new SignInVerificationPageSteps();
     private final NewsPageSteps newsPageSteps = new NewsPageSteps();
-    private final VkApiSteps vkApiSteps = new VkApiSteps();
     private final MyProfilePageSteps myProfilePageSteps = new MyProfilePageSteps();
 
     @Test
@@ -28,30 +27,30 @@ public class VkMyProfileWallPostTest extends BaseTest {
         signInVerificationPageSteps.signInUsingPassword();
         newsPageSteps.clickMyProfileLink();
         User user = new User();
-        vkApiSteps.setUserIdToCurrentByRequest(user);
+        VkApiUtils.setUserIdToCurrentByRequest(user);
         Post post = GetModelInstanceUtils.getNewPostWithRandomMessage(
                 TestDataProvider.getRandomAlphanumericLength());
-        vkApiSteps.createUserWallPostByRequest(user, post);
+        VkApiUtils.createUserWallPostByRequest(user, post);
         myProfilePageSteps.verifyPostMessage(post);
         Photo photo = GetModelInstanceUtils.getNewPhotoWithResource(
                 "testdata/images/cherry-blossom.jpg");
-        vkApiSteps.uploadPhotoToWallUploadServerByRequest(photo,
-                vkApiSteps.getUserPhotosWallUploadServerByRequest(user));
-        vkApiSteps.saveUserWallPhotoByRequest(user, photo);
-        vkApiSteps.editWallPostByRequest(post,
+        VkApiUtils.uploadPhotoToWallUploadServerByRequest(photo,
+                VkApiUtils.getUserPhotosWallUploadServerByRequest(user));
+        VkApiUtils.saveUserWallPhotoByRequest(user, photo);
+        VkApiUtils.editWallPostByRequest(post,
                 RandomUtils.getRandomAlphanumeric(
                         TestDataProvider.getRandomAlphanumericLength()),
-                vkApiSteps.getWallPostAttachmentsStringForPhoto(photo));
+                VkApiUtils.getWallPostAttachmentsStringForPhoto(photo));
         myProfilePageSteps.verifyPostMessage(post);
         myProfilePageSteps.verifyPostPhotoDisplayed(post);
         Comment comment = GetModelInstanceUtils.getNewCommentWithRandomMessage(
                 TestDataProvider.getRandomAlphanumericLength());
-        vkApiSteps.createWallPostCommentByRequest(post, comment);
+        VkApiUtils.createWallPostCommentByRequest(post, comment);
         myProfilePageSteps.clickShowNextCommentLink(post);
         myProfilePageSteps.verifyCommentAuthorUserId(user, comment);
         myProfilePageSteps.clickPostLikeButton(post);
-        vkApiSteps.verifyWallPostLikeUserIdByRequest(user, post);
-        vkApiSteps.deleteWallPostByRequest(post);
-        vkApiSteps.deletePhotoByRequest(photo);
+        VkApiUtils.verifyWallPostLikeUserIdByRequest(user, post);
+        VkApiUtils.deleteWallPostByRequest(post);
+        VkApiUtils.deletePhotoByRequest(photo);
     }
 }
