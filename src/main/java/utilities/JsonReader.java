fa67472193eb284c301d1a64c78f0337a0663448
a.koshelev <a.koshelev@qa-academy.by>
2023-03-21 02:16:43 +0600

    Update to add Apache Commons Text to unescape JSON

--- src/main/java/utilities/JsonReader.java
+++ src/main/java/utilities/JsonReader.java
@@ -3,10 +3,15 @@ package utilities;
 import aquality.selenium.core.utilities.ISettingsFile;
 import aquality.selenium.core.utilities.JsonSettingsFile;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.text.StringEscapeUtils;
 
 @UtilityClass
 public class JsonReader {
     public static ISettingsFile getSettingsResourceFile(String resource) {
         return new JsonSettingsFile(resource);
+    }
+
+    public static String unescapeJson(String json) {
+        return StringEscapeUtils.unescapeJson(json);
     }
 }
