bf24f13230840ca03511261700f5d7d6f54f9573
a.koshelev <a.koshelev@qa-academy.by>
2023-03-21 21:57:06 +0600

    Clear up as there's no need to explicitly use Java 11+ HTTP Client in Selenium anymore, currently all works with Chrome as is

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,15 +3,9 @@ package tests;
 import aquality.selenium.browser.AqualityServices;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import utilities.configuration.Configuration;
 
 public abstract class BaseTest {
-    @BeforeSuite
-    public void beforeSuite() {
-        System.setProperty("webdriver.http.factory", "jdk-http-client");
-    }
-
     @BeforeMethod
     protected void setUp() {
         AqualityServices.getBrowser().goTo(Configuration.getStartUrl());
