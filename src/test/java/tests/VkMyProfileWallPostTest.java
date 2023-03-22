911371e06927a07c61418ba9acb4ca021508a5cf
a.koshelev <a.koshelev@qa-academy.by>
2023-03-23 03:22:23 +0600

    Add line breaks for test steps to improve readability

--- src/test/java/tests/VkMyProfileWallPostTest.java
+++ src/test/java/tests/VkMyProfileWallPostTest.java
@@ -5,12 +5,12 @@ import models.Photo;
 import models.Post;
 import models.User;
 import org.testng.annotations.Test;
-import steps.AuthorizationFormSteps;
-import steps.WallFormSteps;
-import steps.NewsPageSteps;
+import steps.ui.AuthorizationFormSteps;
+import steps.ui.WallFormSteps;
+import steps.ui.NewsPageSteps;
 import utilities.GetModelInstanceUtils;
 import utilities.RandomUtils;
-import utilities.api.VkApiUtils;
+import steps.api.VkApiSteps;
 import utilities.configuration.TestDataProvider;
 
 public class VkMyProfileWallPostTest extends BaseTest {
@@ -18,29 +18,41 @@ public class VkMyProfileWallPostTest extends BaseTest {
     public void testVkMyProfileWallPost() {
         AuthorizationFormSteps.typeLoginAndClickSignInButton(TestDataProvider.getLogin());
         AuthorizationFormSteps.signInUsingPassword(TestDataProvider.getPassword());
+
         NewsPageSteps.clickMyProfileLink();
+
         User user = new User();
-        VkApiUtils.setCurrentUserData(user);
+        VkApiSteps.setCurrentUserData(user);
         Post post = GetModelInstanceUtils.getNewPostWithRandomMessage(TestDataProvider.getRandomAlphanumericLength());
-        VkApiUtils.createUserWallPost(user, post);
+        VkApiSteps.createUserWallPost(user, post);
+
         WallFormSteps.verifyPostMessage(post);
         WallFormSteps.verifyPostAuthorName(user, post);
+
         Photo photo = GetModelInstanceUtils.getNewPhotoWithResource("testdata/images/cherry-blossom.jpg");
-        VkApiUtils.uploadPhotoToWallUploadServer(photo,
-                VkApiUtils.getUserPhotosWallUploadServer(user));
-        VkApiUtils.saveUserWallPhoto(user, photo);
-        VkApiUtils.editWallPost(post, RandomUtils.getRandomAlphanumeric(TestDataProvider.getRandomAlphanumericLength()),
-                VkApiUtils.getWallPostAttachmentsStringForPhoto(photo));
+        VkApiSteps.uploadPhotoToWallUploadServer(photo,
+                VkApiSteps.getUserPhotosWallUploadServer(user));
+        VkApiSteps.saveUserWallPhoto(user, photo);
+        VkApiSteps.editWallPost(post, RandomUtils.getRandomAlphanumeric(TestDataProvider.getRandomAlphanumericLength()),
+                VkApiSteps.getWallPostAttachmentsStringForPhoto(photo));
+
         WallFormSteps.verifyPostMessage(post);
         WallFormSteps.verifyPostPhotoDisplayed(post);
+
         Comment comment = GetModelInstanceUtils.getNewCommentWithRandomMessage(TestDataProvider.getRandomAlphanumericLength());
-        VkApiUtils.createWallPostComment(post, comment);
+        VkApiSteps.createWallPostComment(post, comment);
+
         WallFormSteps.clickShowNextCommentLink(post);
         WallFormSteps.verifyCommentAuthorId(user, comment);
+
         WallFormSteps.clickPostLikeButton(post);
-        VkApiUtils.verifyWallPostLikeUserId(user, post);
-        VkApiUtils.deleteWallPost(post);
+
+        VkApiSteps.verifyWallPostLikeUserId(user, post);
+
+        VkApiSteps.deleteWallPost(post);
+
         WallFormSteps.verifyPostDeleted(post);
-        VkApiUtils.deletePhoto(photo);
+
+        VkApiSteps.deletePhoto(photo);
     }
 }
