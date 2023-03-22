485a4d8ae779a61016e30c44aa8ea63b1d31433f
a.koshelev <a.koshelev@qa-academy.by>
2023-03-23 02:45:23 +0600

    Unite WelcomePageSteps and SignInVerificationPageSteps into AuthorizationFormSteps

---
+++ src/main/java/steps/AuthorizationFormSteps.java
@@ -0,0 +1,29 @@
+package steps;
+
+import lombok.experimental.UtilityClass;
+import org.testng.Assert;
+import pages.NewsPage;
+import pages.SignInVerificationPage;
+import pages.WelcomePage;
+
+@UtilityClass
+public class AuthorizationFormSteps {
+    private static final WelcomePage welcomePage = new WelcomePage();
+    private static final SignInVerificationPage signInVerificationPage = new SignInVerificationPage();
+    private static final NewsPage newsPage = new NewsPage();
+
+    public static void typeLoginAndClickSignInButton(String login) {
+        welcomePage.typeLogin(login);
+        welcomePage.clickSignInButton();
+        Assert.assertTrue(signInVerificationPage.state().waitForDisplayed(),
+                signInVerificationPage.getName() + " is not open");
+    }
+
+    public static void signInUsingPassword(String password) {
+        signInVerificationPage.clickUsePasswordButton();
+        signInVerificationPage.typePassword(password);
+        signInVerificationPage.clickContinueButton();
+        Assert.assertTrue(newsPage.state().waitForDisplayed(),
+                newsPage.getName() + " is not open");
+    }
+}
