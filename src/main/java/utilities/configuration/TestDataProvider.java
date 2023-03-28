cab96eb8ac9e4c993eebe5a53512cc82337cb0fe
a.koshelev <a.koshelev@qa-academy.by>
2023-03-28 11:34:21 +0600

    Add db test data

--- src/main/java/utilities/configuration/TestDataProvider.java
+++ src/main/java/utilities/configuration/TestDataProvider.java
@@ -17,6 +17,10 @@ public class TestDataProvider {
     private static final String testUser = FileReader.getFileContent(
             FileReader.getCanonicalPath(
                     FileReader.getResourceFile("testdata/api/testUser.json")));
+    private static final ISettingsFile dbTablesData =
+            JsonReader.getSettingsResourceFile("testdata/db/dbTablesData.json");
+    private static final ISettingsFile processTestDataTestData =
+            JsonReader.getSettingsResourceFile("testdata/db/processTestDataTestData.json");
 
     public static int getPostId() {
         return (int) getPostWithIdTestData.getValue("/postId");
@@ -44,5 +48,45 @@ public class TestDataProvider {
 
     public static User getTestUser() {
         return JsonReader.getValueTypeFromJson(testUser, User.class);
+    }
+
+    public static String getProjectName() {
+        return dbTablesData.getValue("/projectName").toString();
+    }
+
+    public static String getSessionKey() {
+        return dbTablesData.getValue("/sessionKey").toString();
+    }
+
+    public static Long getBuildNumber() {
+        return Long.valueOf(dbTablesData.getValue("/buildNumber").toString());
+    }
+
+    public static String getAuthorName() {
+        return dbTablesData.getValue("/authorName").toString();
+    }
+
+    public static String getAuthorLogin() {
+        return dbTablesData.getValue("/authorLogin").toString();
+    }
+
+    public static String getAuthorEmail() {
+        return dbTablesData.getValue("/authorEmail").toString();
+    }
+
+    public static String getEnv() {
+        return dbTablesData.getValue("/env").toString();
+    }
+
+    public static String getBrowser() {
+        return dbTablesData.getValue("/browser").toString();
+    }
+
+    public static int getTestsSelectLimit() {
+        return (int) processTestDataTestData.getValue("/testsSelectLimit");
+    }
+
+    public static int getMaxWaitSeconds() {
+        return (int) processTestDataTestData.getValue("/maxWaitSeconds");
     }
 }
