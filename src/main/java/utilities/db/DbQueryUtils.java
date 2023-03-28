4b40c2c10ba4af73b573ed2a6456c55fb1602b3d
a.koshelev <a.koshelev@qa-academy.by>
2023-03-28 11:26:36 +0600

    Add logging for db queries

--- src/main/java/utilities/db/DbQueryUtils.java
+++ src/main/java/utilities/db/DbQueryUtils.java
@@ -1,5 +1,6 @@
 package utilities.db;
 
+import aquality.selenium.browser.AqualityServices;
 import db.DbConnection;
 import lombok.experimental.UtilityClass;
 
@@ -16,6 +17,8 @@ public class DbQueryUtils {
     public static <T> List<T> executeQuery(Function<ResultSet, T> rowMapper, String query, Object... params) {
         try (PreparedStatement preparedStatement = DbConnection.get().prepareStatement(query)) {
             addParams(preparedStatement, params);
+            AqualityServices.getLogger().info("Executing query '%1$s'",
+                    preparedStatement.toString().split(": ", 2)[1]);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 return mapResultSet(resultSet, rowMapper);
             }
@@ -28,6 +31,8 @@ public class DbQueryUtils {
     public static int executeUpdate(String query, Object... params) {
         try (PreparedStatement preparedStatement = DbConnection.get().prepareStatement(query)) {
             addParams(preparedStatement, params);
+            AqualityServices.getLogger().info("Executing update '%1$s'",
+                    preparedStatement.toString().split(": ", 2)[1]);
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new IllegalArgumentException(
@@ -36,6 +41,7 @@ public class DbQueryUtils {
     }
 
     private static void addParams(PreparedStatement preparedStatement, Object[] params) {
+        AqualityServices.getLogger().debug("Adding params to prepared statement");
         IntStream.range(0, params.length)
                 .forEach(i -> {
                     try {
@@ -49,6 +55,7 @@ public class DbQueryUtils {
     }
 
     private static <T> List<T> mapResultSet(ResultSet resultSet, Function<ResultSet, T> rowMapper) {
+        AqualityServices.getLogger().debug("Mapping result set");
         try {
             List<T> results = new ArrayList<>();
             while (resultSet.next()) {
