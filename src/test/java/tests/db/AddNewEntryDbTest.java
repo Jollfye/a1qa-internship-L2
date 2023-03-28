797e6b20f6b531f42031895e6aba5e4005ba5c52
a.koshelev <a.koshelev@qa-academy.by>
2023-03-28 11:39:39 +0600

    Add AddNewEntryDbTest

--- src/test/java/tests/db/AddNewEntryDbTest.java
+++ src/test/java/tests/db/AddNewEntryDbTest.java
@@ -1,11 +1,54 @@
 package tests.db;
 
+import enums.db.TestStatus;
+import models.db.Author;
+import models.db.Project;
+import models.db.Session;
+import models.db.TestModel;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import steps.db.DbSteps;
+import tests.api.GetPostsTest;
+import utilities.configuration.TestDataProvider;
+import utilities.db.DbTimestampUtils;
+
+import java.sql.Timestamp;
 
 public class AddNewEntryDbTest extends BaseDbTest {
+    private Timestamp startTimestamp;
+    private Timestamp endTimestamp;
+
     @Test
     public void dbTestAddNewEntry() {
-        DbSteps.printTestsWhere("1 = 1 LIMIT ?", 10);
+        startTimestamp = DbTimestampUtils.getCurrentTimestampUpToSeconds();
+        GetPostsTest.testGetPosts();
+        endTimestamp = DbTimestampUtils.getCurrentTimestampUpToSeconds();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        Project project = DbSteps.getProjectByName(TestDataProvider.getProjectName());
+        Session session = DbSteps.getSessionBySessionKey(TestDataProvider.getSessionKey());
+        Author author = Author.builder()
+                .name(TestDataProvider.getAuthorName())
+                .login(TestDataProvider.getAuthorLogin())
+                .email(TestDataProvider.getAuthorEmail())
+                .build();
+        author = DbSteps.getMatchingAuthor(author);
+        TestModel test = TestModel.builder()
+                .name(GetPostsTest.class.getSimpleName())
+                .statusId(DbSteps.getStatusByName(TestStatus.PASSED).getId())
+                .methodName(GetPostsTest.class.getMethods()[0].getName())
+                .projectId(project.getId())
+                .sessionId(session.getId())
+                .startTime(startTimestamp)
+                .endTime(endTimestamp)
+                .env(TestDataProvider.getEnv())
+                .browser(TestDataProvider.getBrowser())
+                .authorId(author.getId())
+                .build();
+        DbSteps.addTest(test);
+        DbSteps.verifyTest(test);
+        super.tearDown();
     }
 }
