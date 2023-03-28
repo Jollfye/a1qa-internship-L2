a160dba6c588bd92370ba7c6a8b3d9715d17cb71
a.koshelev <a.koshelev@qa-academy.by>
2023-03-28 11:30:42 +0600

    Add db constants

---
+++ src/main/java/constants/db/DbTable.java
@@ -0,0 +1,9 @@
+package constants.db;
+
+public class DbTable {
+    public static final String TEST = "test";
+    public static final String STATUS = "status";
+    public static final String PROJECT = "project";
+    public static final String SESSION = "session";
+    public static final String AUTHOR = "author";
+}
