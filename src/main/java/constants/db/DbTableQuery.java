8e12b57543232fe32d4dc7108d16a8ad28ffa078
a.koshelev <a.koshelev@qa-academy.by>
2023-03-31 22:58:04 +0600

    Update delete query as there's no need for data filtering except by id

--- src/main/java/constants/db/DbTableQuery.java
+++ src/main/java/constants/db/DbTableQuery.java
@@ -2,11 +2,10 @@ package constants.db;
 
 public class DbTableQuery {
     public static final String SELECT_WHERE = "SELECT * FROM %s WHERE %s";
-    public static final String DELETE_WHERE = "DELETE FROM %s WHERE %s";
+    public static final String DELETE = "DELETE FROM %s WHERE id = ?";
     public static final String INSERT_TEST = "INSERT INTO test (name, status_id, method_name, project_id, session_id, start_time, end_time, env, browser, author_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     public static final String UPDATE_TEST = "UPDATE test SET name = ?, status_id = ?, method_name = ?, project_id = ?, session_id = ?, start_time = ?, end_time = ?, env = ?, browser = ?, author_id = ? WHERE id = ?";
     public static final String INSERT_PROJECT = "INSERT INTO project (name) VALUES (?)";
     public static final String INSERT_SESSION = "INSERT INTO session (session_key, created_time, build_number) VALUES (?, ?, ?)";
-    public static final String UPDATE_SESSION = "UPDATE session SET session_key = ?, created_time = ?, build_number = ? WHERE id = ?";
     public static final String INSERT_AUTHOR = "INSERT INTO author (name, login, email) VALUES (?, ?, ?)";
 }
