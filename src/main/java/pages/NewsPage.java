fbad31c86670c106230800f9e8b44a626524e5ca
a.koshelev <a.koshelev@qa-academy.by>
2023-09-02 21:03:53 +0600

    Update "My profile" link locator

--- src/main/java/pages/NewsPage.java
+++ src/main/java/pages/NewsPage.java
@@ -5,7 +5,7 @@ import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
 public class NewsPage extends Form {
-    private final ILink myProfileLink = getElementFactory().getLink(By.xpath("(//a[@class='LeftMenu__itemLink'])[1]"), "My profile link");
+    private final ILink myProfileLink = getElementFactory().getLink(By.xpath("//li[@id='l_pr']/a"), "My profile link");
 
     public NewsPage() {
         super(By.xpath("(//a[contains(@id,'feed_story')])[1]"), "News page");
